1233f9ad2b7d7345406126b082eb687d8398aee3
ruben <rubendasilva@me.com>
2022-05-15 16:14:25 -0700

    added skills, contact, and projects views, routes, and content

--- myportfolio/businesscard/urls.py
+++ myportfolio/businesscard/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('skills/', views.skills, name='skills'),
+    path('contact', views.contact, name='contact'),
     path('resume/', views.resume, name='resume'),
+    path('projects/', views.projects, name='projects')
     # path('favicon/', )
 ]
--- myportfolio/businesscard/views.py
+++ myportfolio/businesscard/views.py
@@ -9,5 +9,14 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+def skills(request):
+    return render(request, 'skills.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
 def resume(request):
     return render(request, 'resume.html')
+
+def projects(request):
+    return render(request, 'projects.htlm')
