4a77726839b6befbfbe377590659968b7450593e
Ruben <21150133+Roob19@users.noreply.github.com>
2022-06-27 16:29:29 -0700

    heroku

--- myportfolio/myportfolio/settings.py
+++ myportfolio/myportfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == "1"
 
-ALLOWED_HOSTS = [] 
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', os.environ.get('ALLOWED_HOST')] 
 if not DEBUG: 
     ALLOWED_HOSTS += [os.environ.get('ALLOWED_HOST')]
 
